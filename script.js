f144d213d83261547389f8602bf7430743424f44
Adrian Ramirez <47586133+AdrianR1189@users.noreply.github.com>
2024-02-25 17:04:35 -0600

    added media queries and screenshot

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
@@ -17,51 +15,40 @@ let userInput = '';
 
 btn1.addEventListener('click', () => {
   userInput = '1';
-  console.log(userInput);
   return userInput;
 });
 
 btn2.addEventListener('click', () => {
   userInput = '2';
-  console.log(userInput);
-
   return userInput;
 });
 
 btn3.addEventListener('click', () => {
   userInput = '3';
-  console.log(userInput);
-
   return userInput;
 });
 
 btn4.addEventListener('click', () => {
   userInput = '4';
-  console.log(userInput);
-
   return userInput;
 });
 
 btn5.addEventListener('click', () => {
   userInput = '5';
-  console.log(userInput);
-
   return userInput;
 });
 
-// function btnFunction() {
-//   textContent.style.visibility = 'hidden';
-//   starCircle.style.visibility = 'hidden';
-//   numberBtnContainer.style.visibility = 'hidden';
-//   submitBtnContainer.style.visibility = 'hidden';
-// }
 function btnFunction() {
-  mainHide.style.display = 'none';
-  showContainer.style.display = 'flex';
-  showArticle.style.display = 'flex';
-  showP.style.display = 'block';
-  showHeader.style.display = 'block';
-  showRating.style.display = 'flex';
-  showRating.innerHTML = `You selected ${userInput} out of 5`;
+  if (userInput === '') {
+    alert('Please select a rating');
+  } else {
+    mainHide.style.display = 'none';
+    showContainer.style.display = 'flex';
+    showArticle.style.display = 'flex';
+    showP.style.display = 'block';
+    showHeader.style.display = 'block';
+    showRating.style.display = 'flex';
+    showRating.innerHTML = `You selected ${userInput} out of 5`;
+  }
 }
 subBtn.addEventListener('click', btnFunction);
